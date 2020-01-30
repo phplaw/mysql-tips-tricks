181212a81ca83dde415c1460fc047dffead8b05f
Sonny Nguyen <phplaw@gmail.com>
2020-01-31 02:43:59 +0700

    Update update_with_join.sql

--- update_with_join.sql
+++ update_with_join.sql
@@ -1,3 +1,7 @@
 UPDATE ips INNER JOIN country
     ON ips.iso = country.iso
 SET ips.countryid = country.countryid;
+
+-- update
+UPDATE `account_transactions` SET `created` = UNIX_TIMESTAMP(`transaction_datetime`) WHERE created IS NULL;
+
